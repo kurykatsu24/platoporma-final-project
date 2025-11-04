4f06732e4029ba79f7483e763c7d9b94b06154f2
Kurt Allen Alorro <kurtallen.alorro@wvsu.edu.ph>
2025-11-04 14:47:43 +0800

    fix(android): update minSdk to match Flutter requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.platoporma"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21 // Android 5.0 Lollipop
+        minSdk = flutter.minSdkVersion // Android 5.0 Lollipop
         targetSdk = 34   // Android 14
         versionCode = flutter.versionCode
         versionName = flutter.versionName
