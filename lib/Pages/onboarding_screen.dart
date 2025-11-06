ef09b59057e8b0831d31fef4173ef9e25aae7d95
Kurt Allen Alorro <kurtallen.alorro@wvsu.edu.ph>
2025-11-06 16:03:17 +0800

    feat: add spring pop animation to onboarding logo after splash transition

--- lib/Pages/onboarding_screen.dart
+++ lib/Pages/onboarding_screen.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _logoController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Spring pop animation for logo
+    _logoController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    _scaleAnimation = CurvedAnimation(
+      parent: _logoController,
+      curve: Curves.elasticOut,
+    );
+
+    // Start animation after slight delay (to sync with splash fade)
+    Future.delayed(const Duration(milliseconds: 1200), () {
+      _logoController.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _logoController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +48,10 @@ class OnboardingScreen extends StatelessWidget {
         children: [
           // Background image with vignette
           Positioned(
-            left: 40,   
-            top: -30,  
+            left: 40,
+            top: -30,
             child: Transform.scale(
-              scale: 1.2, 
+              scale: 1.2,
               child: ColorFiltered(
                 colorFilter: ColorFilter.mode(
                   Colors.black.withOpacity(0.1),
@@ -33,10 +69,13 @@ class OnboardingScreen extends StatelessWidget {
           // Top logo
           Positioned(
             top: 55,
-            child: Image.asset(
-              'assets/images/platoporma_logo_whitebg1.png',
-              width: 120,
-              height: 120,
+            child: ScaleTransition(
+              scale: _scaleAnimation,
+              child: Image.asset(
+                'assets/images/platoporma_logo_whitebg1.png',
+                width: 110,
+                height: 110,
+              ),
             ),
           ),
 
@@ -54,16 +93,15 @@ class OnboardingScreen extends StatelessWidget {
                 ),
                 boxShadow: [
                   BoxShadow(
-                    color: Color.fromARGB(95, 0, 0, 0), 
-                    blurRadius: 15,  // how soft the shadow looks
+                    color: Color.fromARGB(95, 0, 0, 0),
+                    blurRadius: 15, // how soft the shadow looks
                     spreadRadius: 7, // how large the shadow area is
-                    offset: const Offset(0, 6), // move shadow (X, Y): negative Y = upward shadow
+                    offset: Offset(0, 6), // move shadow (X, Y): negative Y = upward shadow
                   ),
                 ],
               ),
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 2),
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 2),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
@@ -72,7 +110,8 @@ class OnboardingScreen extends StatelessWidget {
                     Transform.translate(
                       offset: const Offset(0, 8), // move upward by 25 pixels (adjust value as needed)
                       child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
+                        padding:
+                            const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
                         decoration: BoxDecoration(
                           color: const Color(0xFFCCEDD8),
                           borderRadius: BorderRadius.circular(20),
@@ -80,54 +119,56 @@ class OnboardingScreen extends StatelessWidget {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Image.asset('assets/images/fork_icon.png', width: 80, height: 80),
+                            Image.asset('assets/images/fork_icon.png',
+                                width: 80, height: 80),
                             const SizedBox(width: 1),
                             Column(
                               children: [
-                              Transform.translate(
-                                offset: const Offset(0, 12),
-                                child: Text(
-                                  'Welcome to',
-                                  style: GoogleFonts.poppins(
-                                    fontSize: 28,
-                                    fontWeight: FontWeight.w600,
-                                    color: const Color(0xFF27453E),
-                                    letterSpacing: -1.5,                              
+                                Transform.translate(
+                                  offset: const Offset(0, 12),
+                                  child: Text(
+                                    'Welcome to',
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w600,
+                                      color: const Color(0xFF27453E),
+                                      letterSpacing: -1.5,
+                                    ),
                                   ),
                                 ),
-                              ),  
-                              Transform.translate(
-                                offset: const Offset(0, -5),                     
-                                child: RichText(
-                                  text: TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: 'Plato',
-                                        style: GoogleFonts.poppins(
-                                          fontSize: 44,
-                                          fontWeight: FontWeight.bold,
-                                          color: const Color(0xFF27453E),
-                                          letterSpacing: -2,
+                                Transform.translate(
+                                  offset: const Offset(0, -5),
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Plato',
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 44,
+                                            fontWeight: FontWeight.bold,
+                                            color: const Color(0xFF27453E),
+                                            letterSpacing: -2,
+                                          ),
                                         ),
-                                      ),
-                                      TextSpan(
-                                        text: 'Porma',
-                                        style: GoogleFonts.poppins(
-                                          fontSize: 44,
-                                          fontStyle: FontStyle.italic,
-                                          fontWeight: FontWeight.bold,
-                                          color: const Color(0xFF27453E),
-                                          letterSpacing: -2,
+                                        TextSpan(
+                                          text: 'Porma',
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 44,
+                                            fontStyle: FontStyle.italic,
+                                            fontWeight: FontWeight.bold,
+                                            color: const Color(0xFF27453E),
+                                            letterSpacing: -2,
+                                          ),
                                         ),
-                                      ),
-                                    ],
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ),
                               ],
                             ),
                             const SizedBox(width: 1),
-                            Image.asset('assets/images/spoon_icon.png', width: 80, height: 80),
+                            Image.asset('assets/images/spoon_icon.png',
+                                width: 80, height: 80),
                           ],
                         ),
                       ),
@@ -143,7 +184,7 @@ class OnboardingScreen extends StatelessWidget {
                         fontStyle: FontStyle.italic,
                         fontWeight: FontWeight.w500,
                         color: const Color(0xFF27453E),
-                        letterSpacing: -1,        
+                        letterSpacing: -1,
                       ),
                     ),
                     const SizedBox(height: 20),
@@ -157,7 +198,7 @@ class OnboardingScreen extends StatelessWidget {
                         fontStyle: FontStyle.italic,
                         fontWeight: FontWeight.w500,
                         color: const Color(0xFF27453E),
-                        letterSpacing: -1,        
+                        letterSpacing: -1,
                       ),
                     ),
                     const SizedBox(height: 45),
