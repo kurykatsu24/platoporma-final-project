2711136d8ee610b308aac1c5270d09483a93aadf
Christine Joy Maravilla <christinejoy.maravilla@wvsu.edu.ph>
2025-11-10 15:25:49 +0800

    feat(login): enhance login completion screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:platoporma/Pages/signup_completion_screen.dart';
+import 'package:platoporma/Pages/login_completion_screen.dart';
 
 Future<void> main() async {
   // Ensures Flutter widgets are ready before running any async code
@@ -9,7 +9,8 @@ Future<void> main() async {
   // Initialize Supabase
   await Supabase.initialize(
     url: 'https://qyhvkifjstjwlbbswdoc.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5aHZraWZqc3Rqd2xiYnN3ZG9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMjY4MDYsImV4cCI6MjA3NzgwMjgwNn0.yVxdM0vD0jKe34XlxvGpvY9vaxCC665JRfzNBlMdlTE',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5aHZraWZqc3Rqd2xiYnN3ZG9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMjY4MDYsImV4cCI6MjA3NzgwMjgwNn0.yVxdM0vD0jKe34XlxvGpvY9vaxCC665JRfzNBlMdlTE',
   );
 
   // Run your app
@@ -22,13 +23,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       title: 'Platoporma',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       // Set SplashScreen as the home
-      home: SignUpCompletionScreen(),
+      home: LoginCompletionScreen(),
     );
   }
 }
